fd3aafc5a5091325078e3d8db08c0e07a9aced8a
longdv ph29867 <emchuaroinhe@gmail.com>
2024-02-02 21:23:14 +0700

    update

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-600">Hello</div>
+    </>
+  );
+}
+
+export default App;
